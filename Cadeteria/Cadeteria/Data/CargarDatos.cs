97646e584df6db58abf3bfc05778c7b272d7564b
VictorNaitoon <victormolinalvp@gmail.com>
2023-09-30 18:13:53 -0300

    modifique algunas cosas de la lectura de los archivos csv

--- Cadeteria/Cadeteria/Data/CargarDatos.cs
+++ Cadeteria/Cadeteria/Data/CargarDatos.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cadeteria.Modelos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,12 +26,68 @@ namespace Cadeteria.Data
                     }
                 }
                 sr.Close();
-            } catch(Exception ex)
+            } catch (Exception ex)
             {
                 throw new Exception(ex.Message);
             }
 
             return lista;
+        }
+
+        public List<Cadete> LeerCSVCadetes(string ruta)
+        {
+            int autoincrementar = 0;
+            List<Cadete> listadoCadetes = new List<Cadete>();
+            try
+            {
+                var archivo = new FileStream(ruta, FileMode.Open);
+                var sr = new StreamReader(archivo);
+                var linea = "";
+                while ((linea = sr.ReadLine()) != null)
+                {
+                    string[] campos = linea.Split(",");
+                    List<Pedido> listadoPedidos = new List<Pedido>();
+
+                    Cadete cadete = new Cadete(autoincrementar++, campos[0], campos[2], campos[1], listadoPedidos);
+
+                    listadoCadetes.Add(cadete);
+                }
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+
+            return listadoCadetes;
+        }
+
+        public List<SistemaCadeteria> LeerCSVCadetereias(string ruta)
+        {
+            List<SistemaCadeteria> listadoCadeterias = new List<SistemaCadeteria>();
+            try
+            {
+                var archivo = new FileStream(ruta, FileMode.Open);
+                var sr = new StreamReader(archivo);
+                var linea = "";
+                while ((linea = sr.ReadLine()) != null)
+                {
+                    var campos = linea.Split(",");
+                    List<Cadete> listadoCadetes = new List<Cadete>();
+
+                    SistemaCadeteria cadeteria = new SistemaCadeteria(campos[0], campos[1], listadoCadetes);
+
+                    listadoCadeterias.Add(cadeteria);
+                }
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return listadoCadeterias;
         }
     }
 }
