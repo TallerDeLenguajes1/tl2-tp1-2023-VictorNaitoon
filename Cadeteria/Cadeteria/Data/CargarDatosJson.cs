1a3b3ace9fb3e1b02a3294729fc24a8be445fe7f
VictorNaitoon <victormolinalvp@gmail.com>
2023-11-01 23:53:33 -0300

    agregue la clase para leer los datos de un json y funcionaba, trabajo terminado

---
+++ Cadeteria/Cadeteria/Data/CargarDatosJson.cs
@@ -0,0 +1,28 @@
+﻿using Cadeteria.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cadeteria.Data
+{
+    public class CargarDatosJson : AccesoADatos
+    {
+        public override List<Cadete> LeerArchivoCadetes(string ruta)
+        {
+            string archivo = File.ReadAllText(ruta);
+            List<Cadete> Cadetes = JsonSerializer.Deserialize<List<Cadete>>(archivo);
+            return Cadetes;
+        }
+
+        public override List<SistemaCadeteria> LeerArchivoCadeterias(string ruta)
+        {
+            string archivo = File.ReadAllText(ruta);
+            List<SistemaCadeteria> Cadeterias = JsonSerializer.Deserialize<List<SistemaCadeteria>>(archivo);
+            return Cadeterias;
+        }
+    }
+}
